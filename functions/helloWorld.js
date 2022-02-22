d352163988e7f07457b29f8327634137a630a30a
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-22 16:50:34 +0300

    minor change on what is returned by helloworld

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context, callback) => {
   return {
     statusCode: 200,
-    body: { message: 'hello world' },
+    body: JSON.stringify({ message: 'hello world' }),
   };
 };
